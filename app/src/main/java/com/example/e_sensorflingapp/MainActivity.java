9492dde0a3178607e77a4eafeb9fc653e3832c75
damnenby <damnenby@gmail.com>
2026-05-25 14:09:48 +0200

    add restart state

--- app/src/main/java/com/example/e_sensorflingapp/MainActivity.java
+++ app/src/main/java/com/example/e_sensorflingapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.e_sensorflingapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewHighscore;
+    private TextView textViewLastAttempt;
+    private ProgressBar progressBarHighscore;
+    private int highscore;
+    private int lastAttempt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
+        updateScoreViews();
+    }
+
+    private void initViews() {
+        textViewHighscore = findViewById(R.id.textViewHighscore);
+        textViewLastAttempt = findViewById(R.id.textViewLastAttempt);
+        progressBarHighscore = findViewById(R.id.progressBarHighscore);
+        Button buttonRestart = findViewById(R.id.buttonRestart);
+
+        progressBarHighscore.setMax(FlingCalculator.MAX_SCORE);
+        buttonRestart.setOnClickListener(v -> resetHighscore());
+    }
+
+    private void resetHighscore() {
+        highscore = 0;
+        lastAttempt = 0;
+        updateScoreViews();
+    }
+
+    private void updateScoreViews() {
+        textViewHighscore.setText(getString(R.string.highscore_value, highscore));
+        textViewLastAttempt.setText(getString(R.string.last_attempt_value, lastAttempt));
+        progressBarHighscore.setProgress(highscore);
     }
 }
